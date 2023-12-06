0ee6ed29150c01e7d4376f269a0d8eca97ec3c43
Kshitiz-KBen <kshitizsingh004@gmail.com>
2023-12-06 17:09:03 +0530

    added the complete the authentication

--- Login/src/index.js
+++ Login/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require("path");
-const bcrypt = require("bcrypt");
 const collection = require("./config");
 
 const app = express();
+// converting data into json format
+app.use(express.json());
 
-// use EJS as the viw engine app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+// using EJS as the viw engine app.set('view engine', 'ejs');
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
@@ -16,6 +19,39 @@ app.get("/", (req, res) => {
 
 app.get("/signup", (req, res) => {
     res.render("signup");
+});
+
+// Registering User
+app.post("/signup", async (req, res) => {
+const data = {
+    name: req.body.username,
+    password: req.body.password
+}
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+    
+});
+
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+
+        if (!check) {
+            res.send("User name cannot be found");
+            return;
+        }
+
+        // Comparing the plaintext password from the request with the stored password
+        if (req.body.password === check.password) {
+            res.render("home");
+        } else {
+            res.send("Wrong password");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.send("Something went wrong");
+    }
 });
 
 const port = 5000;
