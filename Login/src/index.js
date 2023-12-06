9224fc628cf7bafde707bc4b3298f77b3dd1eb8f
Kshitiz-KBen <kshitizsingh004@gmail.com>
2023-12-06 16:22:11 +0530

    Created the login form & styled accordingly

---
+++ Login/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const path = require("path");
+
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+// use EJS as the viw engine app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+const port = 5000;
+ app.listen(port, () => { console.log(`Server running on Port:${port}`);
+})
